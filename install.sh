a2294dfbd8e72310a90f75167324732789c1db39
James Crane <james@cranesnest.us>
2019-02-16 14:29:47 -0600

    Updated install instructions.

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Place a file with name "ssh" onto the root of SD card. File can be empty.
+#
+# SSH pi@raspberrypi.local -> Configure with raspi-config:
+# hostname -> "vision"
+# `shutdown -r now`
+#
+# SSH pi@vision.local -> Run this file on the pi:
+# `sh install.sh`
 
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
